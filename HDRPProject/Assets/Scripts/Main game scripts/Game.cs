e2b871abbc73a187232720e5d14f96e5cf28907a
adamdeere <adam.deere@hotmail.com>
2020-09-11 09:19:25 +0100

    cheeky little commit
    small updates

--- HDRPProject/Assets/Scripts/Main game scripts/Game.cs
+++ HDRPProject/Assets/Scripts/Main game scripts/Game.cs
@@ -1,9 +1,14 @@
 ﻿using SaveSystemScripts;
+using UnityEngine;
 
 namespace Main_game_scripts
 {
     public class Game : PersistantObject
     {
+        //used to create a shape. needs deleting when finished with
+        
+
+        [SerializeField] private Transform prefab;
         // Start is called before the first frame update
         void Start()
         {
@@ -13,7 +18,12 @@ namespace Main_game_scripts
         // Update is called once per frame
         void Update()
         {
-        
+            
+        }
+        void CreateObject () 
+        {
+            Transform t = Instantiate(prefab);
+            t.localPosition = Random.insideUnitSphere * 5f;
         }
     }
 }
