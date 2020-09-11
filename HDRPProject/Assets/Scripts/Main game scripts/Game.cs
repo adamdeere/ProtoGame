74e4d28df17f12f9338fc2b0aa8bf20a8a443a82
adamdeere <adam.deere@hotmail.com>
2020-09-11 09:44:14 +0100

    Update Game.cs

--- HDRPProject/Assets/Scripts/Main game scripts/Game.cs
+++ HDRPProject/Assets/Scripts/Main game scripts/Game.cs
@@ -1,14 +1,25 @@
-﻿using SaveSystemScripts;
+﻿using System;
+using SaveSystemScripts;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Main_game_scripts
 {
     public class Game : PersistantObject
     {
-        //used to create a shape. needs deleting when finished with
-        
-
+        private InputPrefs _input;
         [SerializeField] private Transform prefab;
+
+        private void Awake()
+        {
+            _input = new InputPrefs();
+        }
+
+        private void OnEnable()
+        {
+            
+        }
+
         // Start is called before the first frame update
         void Start()
         {
