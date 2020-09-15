69d25db025dbf1950d24e59d966923b85d06b720
adamdeere <adam.deere@hotmail.com>
2020-09-15 11:56:03 +0100

    click functions sorted
    click functions to the buttons have been added

---
+++ HDRPProject/Assets/Scripts/IntroClickManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class IntroClickManager : MonoBehaviour
+{
+   public void StartClickPressed()
+   {
+      Debug.Log("start button pressed");
+   }
+   public void LoadClickPressed()
+   {
+      Debug.Log("load button pressed");
+   }
+}
